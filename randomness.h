181b17b7d9910f5e0da0589ca625a4bed6af309a
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2023-04-09 13:31:41 +0200

    Initial import

---
+++ randomness.h
@@ -0,0 +1,13 @@
+/*
+ *  SPDX-License-Identifier: MIT
+ */
+
+#ifndef RANDOMNESS_H
+#define RANDOMNESS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int rand_bits(uint8_t* dst, size_t num_bits);
+
+#endif
