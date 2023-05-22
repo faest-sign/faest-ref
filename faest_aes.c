eb2f507da723a9a8c70643b1050ab6a43a594468
Shibam Mukherjee <shibam.mukherjee@iaik.tugraz.at>
2023-05-22 20:17:47 +0200

    .

--- faest_aes.c
+++ faest_aes.c
@@ -30,7 +30,11 @@ int aes_key_schedule_forward(uint32_t lambda, uint32_t m, const uint8_t* x, uint
 
   // STep: 2..3
   // TODO, change to field type
-  uint8_t* y = malloc((numround + 1) * 16 * 16);
+  if (m == 1) {
+    y = malloc((numround + 1) * 16);
+  } else {
+    y = malloc((numround + 1) * lambdaByte);
+  }
   memcpy(y, x, 16);
 
   // Step: 4
@@ -52,9 +56,180 @@ int aes_key_schedule_forward(uint32_t lambda, uint32_t m, const uint8_t* x, uint
   return 1;
 }
 
-// w (extended witness bits), u (vole masks), v (vole tags), in (faest public-key input block bits),
-// out (faest public-key output block bits), chal (quicksilver challenge), a_tilde and b_tilde
-// (quicksilver response)
+int aes_key_schedule_backward(uint32_t lambda, uint32_t m, const uint8_t* x, const uint8_t* xk,
+                              uint8_t Mtag, uint8_t Mkey, const uint8_t* delta, uint8_t* y) {
+
+  uint32_t lambdaByte = lambda / 8;
+  uint32_t numround   = 0;
+  // TODO: verify if they mean the word size N is the key size and not the standard block size
+  uint32_t N = lambdaByte / 4;
+  switch (lambda) {
+  case 256:
+    numround = 14;
+    break;
+  case 192:
+    numround = 12;
+  default:
+    numround = 10;
+    break;
+  }
+
+  // Step: 1
+  if ((Mtag == 1 && Mkey == 1) || (Mkey == 1 && delta == NULL)) {
+    return 0;
+  }
+
+  // STep: 2
+  // TODO: change to field type
+  uint8_t* y;
+  // TODO: what is S_ke_Byte ??
+  uint32_t S_ke_Byte = 8;
+  if (m == 1) {
+    y = malloc(S_ke_Byte);
+  } else {
+    y = malloc(lambdaByte * S_ke_Byte);
+  }
+
+  // Step: 3..4
+  uint32_t i_wd = 0;
+  uint32_t c    = 0;
+
+  // Step 5
+  uint8_t rmvRcon = 1;
+  uint8_t i_rcon  = 0;
+
+  // Step: 6..13
+  uint8_t x_tilde;
+  for (uint32_t j = 0; j < S_ke_Byte; j++) {
+    x_tilde = x[j] - xk[i_wd + c];
+    // TODO: check the strange sign
+    if (Mtag == 0 && rmvRcon == 1 && c == 0) {
+      // TODO: rcon = toBits()
+      i_rcon = i_rcon + 1;
+      uint8_t r;
+      // TODO: r = 0||rcon * ((Mkey * delta) + (1_f_2^m - Mkey))
+      x_tilde = x_tilde - r;
+    }
+
+    // Step: 14..24
+    // TODO: unsure how to combine F_(2^m)^8 when m = 1 or m = lambdaByte
+    uint8_t* y_tilde;
+    if (m == 1) {
+      y_tilde = malloc(1);
+    } else {
+      y_tilde = malloc(lambdaByte * 8);
+    }
+    // TODO
+    y[j] = y_tilde[j];
+    c    = c + 1;
+
+    // Step: 25..30
+    if (c == 4) {
+      c = 0;
+      if (lambda == 192) {
+        i_wd = i_wd + 24;
+      } else {
+        i_wd = i_wd + 16;
+        if (lambda == 256) {
+          // TODO: hopefully the sign is fine
+          rmvRcon == !rmvRcon;
+        }
+      }
+    }
+  }
+
+  // Step: 5..10
+  // TODO, check if this is not N * (numround + 1) ?
+  for (uint32_t j = N; j < 4 * (numround + 1); j++) {
+    if (j % N == 0 || (N > 6 && j % N == 4)) {
+      // TODO: doing as per the byte thingy, hope its no problem
+      memcpy(y + (4 * N), x + i_wd, 4);
+      i_wd += 4;
+    } else {
+      for (uint32_t i = 0; i < 4; i++) {
+        y[(4 * j) - i] = y[4 * (j - N) + i] + y[4 * (j - 1) + i];
+      }
+    }
+  }
+  return 1;
+}
+
+int aes_key_schedule_constraints(uint32_t lambda, const uint8_t* w, const uint8_t* v,
+                                 const uint8_t Mkey, const uint8_t* q, const uint8_t* delta,
+                                 uint8_t** A, uint8_t* k, uint8_t* vk, uint8_t* B, uint8_t* qk) {
+
+  uint32_t lambdaByte = lambda / 8;
+
+  if (Mkey == 0) {
+
+    // STep: 2
+    uint8_t *k, vk, w_dash, v_w_dash;
+    aes_key_schedule_forward(lambda, 1, w, 0, 0, NULL, k);
+
+    // Step: 3
+    aes_key_schedule_forward(lambda, lambda, v, 1, 0, NULL, vk);
+
+    // Step: 4
+    uint8_t* w_lambda = malloc(sizeof(w) - lambdaByte);
+    memcpy(w_lambda, w + lambdaByte, sizeof(w) - lambdaByte);
+    aes_key_schedule_backward(lambda, 1, w_lambda, k, 0, 0, NULL, w_dash);
+
+    // Step: 5
+    uint8_t* v_lambda = malloc(sizeof(v) - lambdaByte);
+    memcpy(v_lambda, v + lambdaByte, sizeof(v) - lambdaByte);
+    aes_key_schedule_backward(lambda, lambda, v_lambda, vk, 1, 0, NULL, v_w_dash);
+
+    // Step: 6..8
+    // TODO: what is S_ke ?
+    uint32_t S_ke, N;
+    for (uint32_t j = 0; j < S_ke / 4; j++) {
+      uint32_t i = lambda - 32 + (j * (lambda / floor(N / 4)));
+
+      for (uint32_t r = 0; r <= 3; r) {
+        // TODO: line 9..12
+      }
+
+      // Step: 13..15
+      for (uint32_t r = 0; r <= 3; r) {
+        // TODO: line 14..15
+      }
+    }
+
+    return 1; // returninig from branch 1
+  } else {
+
+    // Step: 18..19
+    aes_key_schedule_forward(lambda, lambda, q, 0, 1, delta, qk);
+    uint8_t* q_w_dash;
+    uint8_t* q_lambda = malloc(sizeof(q) - lambdaByte);
+    memcpy(q_lambda, q + lambdaByte, lambdaByte);
+    // TODO: check if it is indeed null ?
+    aes_key_schedule_backward(lambda, lambda, q_lambda, NULL, 0, 1, delta, q_w_dash);
+
+    // Step 20..22
+    // TODO: fix s_ke
+    uint32_t S_ke, N;
+    for (uint32_t j = 0; j < S_ke / 4; j++) {
+      uint32_t i = lambda - 32 + (j * (lambda / floor(N / 4)));
+
+      for (uint32_t r = 0; r <= 3; r++) {
+        // TODO: steps 23..24
+      }
+
+      // STep: 25
+      for (uint32_t r = 0; r <= 3; r++) {
+        // TODO: step 26
+      }
+    }
+
+    return 2; // returnining from brach 2
+  }
+  return 0; // error
+}
+
+// w (extended witness bits), u (vole masks), v (vole tags), in (faest public-key input block
+// bits), out (faest public-key output block bits), chal (quicksilver challenge), a_tilde and
+// b_tilde (quicksilver response)
 void aes_prove(uint8_t* w, uint8_t* u, uint8_t** v, uint8_t* in, uint8_t* out, uint8_t* chal,
                uint32_t lambda, uint32_t tau, uint32_t l, uint8_t* a_tilde, uint8_t* b_tilde) {
 
