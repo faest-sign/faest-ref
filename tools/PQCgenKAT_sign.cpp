9abb2e5f2b09252719c51c7ab1a49838c91c41be
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2023-05-23 20:05:41 +0200

    Refactor

--- tools/PQCgenKAT_sign.cpp
+++ tools/PQCgenKAT_sign.cpp
@@ -1,14 +1,13 @@
 
 #include <algorithm>
+#include <array>
+#include <boost/io/ios_state.hpp>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
 #include <string>
 #include <string_view>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <array>
 #include <vector>
-#include <boost/io/ios_state.hpp>
 
 #include "rng.h"
 extern "C" {
@@ -17,39 +16,22 @@ extern "C" {
 
 #define KAT_SUCCESS 0
 #define KAT_FILE_OPEN_ERROR -1
-#define KAT_DATA_ERROR -3
 #define KAT_CRYPTO_FAILURE -4
 
 namespace {
-  constexpr std::string_view AlgName{CRYPTO_ALGNAME};
-
   template <typename Iterator>
   void print_hex(std::ostream& os, Iterator begin, Iterator end) {
     boost::io::ios_flags_saver ifs{os};
     os << std::hex << std::setfill('0');
     std::for_each(begin, end, [&](unsigned int c) { os << std::setw(2) << c; });
   }
+
+  constexpr size_t num_tvs = 100;
 } // namespace
 
 int main() {
-  std::ostringstream oss1;
-  oss1 << "PQCsignKAT_" << AlgName << ".req";
-  const auto fn_req = oss1.str();
-
-  std::ostringstream oss2;
-  oss2 << "PQCsignKAT_" << AlgName << ".rsp";
-  const auto fn_rsp = oss2.str();
-
-  std::ofstream fp_req{fn_req};
-  if (!fp_req) {
-    std::cout << "Couldn't open <" << fn_req << "> for write" << std::endl;
-    return KAT_FILE_OPEN_ERROR;
-  }
-  std::ofstream fp_rsp{fn_rsp};
-  if (!fp_rsp) {
-    std::cout << "Couldn't open <" << fn_rsp << "> for write" << std::endl;
-    return KAT_FILE_OPEN_ERROR;
-  }
+  constexpr std::string_view fn_req = "PQCsignKAT_" CRYPTO_ALGNAME ".req";
+  constexpr std::string_view fn_rsp = "PQCsignKAT_" CRYPTO_ALGNAME ".rsp";
 
   {
     std::array<uint8_t, 48> entropy_input;
@@ -60,35 +42,47 @@ int main() {
 
   std::vector<std::array<uint8_t, 48>> seeds;
   std::vector<std::vector<uint8_t>> messages;
-  seeds.reserve(100);
-  messages.reserve(100);
+  seeds.reserve(num_tvs);
+  messages.reserve(num_tvs);
 
-  for (unsigned int i = 0; i < 100; i++) {
-    std::array<uint8_t, 48> seed;
-    randombytes(seed.data(), 48);
-    seeds.emplace_back(seed);
+  {
+    std::ofstream fp_req{fn_req.data()};
+    if (!fp_req) {
+      std::cout << "Couldn't open <" << fn_req << "> for write" << std::endl;
+      return KAT_FILE_OPEN_ERROR;
+    }
+    for (unsigned int i = 0; i < num_tvs; i++) {
+      std::array<uint8_t, 48> seed;
+      randombytes(seed.data(), 48);
+      seeds.emplace_back(seed);
 
-    fp_req << "count = " << i << "\n";
-    fp_req << "seed = ";
-    print_hex(fp_req, seed.begin(), seed.end());
-    fp_req << "\n";
+      fp_req << "count = " << i << "\n";
+      fp_req << "seed = ";
+      print_hex(fp_req, seed.begin(), seed.end());
+      fp_req << "\n";
 
-    const size_t mlen = 33 * (i + 1);
-    fp_req << "mlen = " << mlen << "\n";
+      const size_t mlen = 33 * (i + 1);
+      fp_req << "mlen = " << mlen << "\n";
 
-    std::vector<uint8_t> msg;
-    msg.resize(mlen);
-    randombytes(msg.data(), mlen);
-    fp_req << "msg = ";
-    print_hex(fp_req, msg.begin(), msg.end());
-    fp_req << "\n";
-    messages.emplace_back(msg);
+      std::vector<uint8_t> msg;
+      msg.resize(mlen);
+      randombytes(msg.data(), mlen);
+      fp_req << "msg = ";
+      print_hex(fp_req, msg.begin(), msg.end());
+      fp_req << "\n";
+      messages.emplace_back(msg);
 
-    fp_req << "pk =\nsk = \nsmlen =\nsm=\n" << std::endl;
+      fp_req << "pk =\nsk = \nsmlen =\nsm=\n" << std::endl;
+    }
   }
 
-  fp_rsp << "# " << CRYPTO_ALGNAME << "\n\n";
-  for (unsigned int i = 0; i != seeds.size(); ++i) {
+  std::ofstream fp_rsp{fn_rsp.data()};
+  if (!fp_rsp) {
+    std::cout << "Couldn't open <" << fn_rsp << "> for write" << std::endl;
+    return KAT_FILE_OPEN_ERROR;
+  }
+  fp_rsp << "# " CRYPTO_ALGNAME "\n\n";
+  for (unsigned int i = 0; i != num_tvs; ++i) {
     auto& seed      = seeds[i];
     const auto& msg = messages[i];
 
