60bcfe11ac8a6ecdeee7bf1da69e1a468e2c9a24
Shibam Mukherjee <shibam.mukherjee@iaik.tugraz.at>
2023-05-26 19:50:33 +0200

    ...

--- faest_aes.c
+++ faest_aes.c
@@ -171,6 +171,7 @@ int aes_key_schedule_backward(uint32_t lambda, uint32_t R, uint32_t Nwd, uint32_
       }
 
       if (Mtag == 0 && rmvRcon == true && c == 0) {
+        // TODO: different r's !!!!
         uint8_t r   = Rcon[ircon];
         ircon       = ircon + 1;
         bf8_t* bf_r = malloc(8);
@@ -798,101 +799,90 @@ void aes_prove(uint8_t* w, uint8_t* u, uint8_t** V, uint8_t* in, uint8_t* out, u
   zk_hash_128(b_tilde, r, chal, t, a0_vs_concat, l);
 }
 
-bool aes_verify(uint8_t* d, uint8_t* Q, uint8_t* chal_2, uint8_t* chal_3, uint8_t* a_tilde,
+bool aes_verify(uint8_t* d, uint8_t** Q, uint8_t* chal_2, uint8_t* chal_3, uint8_t* a_tilde,
                 uint8_t* b_tilde, uint8_t* in, uint8_t* out, uint32_t lambda, uint32_t tau,
-                uint32_t l, uint32_t k0, uint32_t k1) {
+                uint32_t l, uint32_t beta, uint32_t R, uint32_t Nwd, uint32_t Ske, uint32_t Lke,
+                uint32_t Lenc, uint32_t Senc, uint32_t C, uint32_t k0, uint32_t k1, uint32_t t0,
+                uint32_t t1) {
 
-    // Step: 1
-  // TODO: confirm if this is a check or we actually assign it here
-  uint32_t t0 = lambda % tau;
-  uint32_t t1 = (lambda - (k0 * t0)) / k1;
+  uint32_t lambdaBytes = lambda / 8;
+  uint32_t d_len       = l;
+  uint32_t Q_len       = tau;
+  uint32_t Qi_len      = (l + lambda) + lambda;
+  uint32_t chal_2_len  = 3 * lambda + 64;
+  uint32_t chal_3_len  = lambda;
+  uint32_t in_len      = beta * 128;
+  uint32_t out_len     = beta * 128;
 
-  // STep: 2
-  // TODO: field operation
-  uint8_t* delta;
+  bf128_t bf_delta = bf128_load(chal_3);
+  uint8_t* delta   = malloc(16);
+  memcpy(delta, chal_3, 16);
 
-  // Step: 3
-  uint8_t* in0 = malloc(16);
-  memcpy(in0, in, 16);
-  uint8_t* out0 = malloc(16);
-  memcpy(out0, out, 16);
+  uint8_t* in_0  = malloc(16);
+  uint8_t* out_0 = malloc(16);
+  memcpy(in_0, in, 16);
+  memcpy(out_0, out, 16);
 
-  // STep: 4
-  // TODO: What is B ?
-  uint32_t B = 2;
-  uint8_t *in1, out1;
-  if (B == 2) {
-    in1  = malloc(16);
-    out1 = malloc(16);
+  uint8_t* in_1  = malloc(16);
+  uint8_t* out_1 = malloc(16);
+  if (beta == 2) {
+    memcpy(in_1, in + 16, 16);
+    memcpy(out_1, out + 16, 16);
   }
 
-  // Step: 5..9
-  // TODO: from where comes the small b ?
-  // TODO:
-  uint8_t* b;
-  uint32_t kb = 0;
-  uint8_t** q;
+  uint32_t kb;
   for (uint32_t i = 0; i < tau; i++) {
     if (i < t0) {
-      b  = 0;
       kb = k0;
     } else {
-      b  = 1;
       kb = k1;
     }
-    uint8_t* chalout;
-    ChalDec(chal_3, i, k0, t0, k1, t1, chalout);
+    uint8_t* d = malloc(kb);
+    ChalDec(chal_3, i, k0, t0, k1, t1, d);
+
     for (uint32_t j = 0; j < kb; j++) {
-      // TODO: check it, very likely it is wrong, need to do bf multiplication I guess
-      q[i][j] = q[i][j] ^ (chalout[j] * d[j]);
+      // TODO: have a look what is happeninig here !!
     }
   }
-  // Step: 10..12
-  for (uint32_t i = 0; i < lByte + tau; i++) {
-    // TODO: ToField problem....
+
+  bf128_t* bf_q  = malloc(sizeof(bf128_t) * (l + tau));
+  uint8_t* q_lke = malloc(lambdaBytes * (Lke));
+  for (uint32_t i = 0; i < l + tau; i++) {
+    bf_q[i] = bf128_load(Q[i]);
   }
-  // TODO: sampling b
+  memcpy(q_lke, bf_q, sizeof(bf128_t) * Lke);
 
-  // Step: 13
-  // TODO: pass and check for null instead of 0 ?
-  // TODO:
-  uint32_t Mkey = 1;
-  uint32_t lByte_ke;
-  uint8_t* q_;
-  uint8_t* b1;
-  uint8_t* qk;
-  memcpy(q_, q, lByte_ke);
-  aes_key_schedule_constrains(lambda, NULL, NULL, Mkey, q_, delta, b1, qk);
+  bf128_t* bf_b = malloc(sizeof(bf128_t) * C);
 
-  // Step: 14
-  // TODO
-  uint8_t* q_1;
-  uint32_t lByte_enc;
-  uint8_t* b2;
-  memcpy(q_1, q + lByte_ke, lByte_enc);
-  aes_cipher_constrains(lambda, in0, out0, NULL, NULL, NULL, NULL, Mkey, q_1, qk, delta, b2);
+  uint8_t *A0, *A1, *k, *vk, *B_0, *qk;
+  aes_key_schedule_constraints(lambda, R, Nwd, Ske, Lke, NULL, NULL, 1, q_lke, delta, A0, A1, k, vk,
+                               B_0, qk);
 
-  // Step: 15..17
-  // TODO
-  if (B == 1) {
-    memcpy(b, b1, sizeof(b1));
-    memcpy(b + sizeof(b1), b2, sizeof(b2));
+  uint8_t *A0_1, *A1_1, *B_1;
+  uint8_t* q_lke_lenc = malloc(lambdaBytes * (Lenc - Lke));
+  memcpy(q_lke_lenc, bf_q + Lke, sizeof(bf128_t) * (Lenc - Lke));
+  aes_enc_constraints(lambda, R, Lenc, Senc, in_0, out_0, NULL, NULL, NULL, NULL, 1, q_lke_lenc, qk,
+                      delta, A0_1, A1_1, B_1);
+
+  uint8_t* B;
+  if (beta == 1) {
+    B = malloc(16 * R * 2);
+    memcpy(B, B_0, 16 * R);
+    memcpy(B + (R * 16), B_1, 16 * R);
   } else {
-    uint8_t* b3;
-    uint8_t* q_2;
-    memcpy(q_2, q + (lByte_ke + lByte_enc), lByte - (lByte_ke + lByte_enc));
-    aes_cipher_constrains(lambda, in1, out1, NULL, NULL, NULL, NULL, Mkey, q_2, qk, delta, b3);
-    memcpy(b, b1, sizeof(b1));
-    memcpy(b + sizeof(b1), b2, sizeof(b2));
-    memcpy(b + sizeof(b1) + sizeof(b2), b3, sizeof(b3));
+    uint8_t *A0_2, *A1_2, *B_2;
+    uint8_t* q_lke_enc_l = malloc(lambdaBytes * (l - (Lke + Lenc)));
+    memcpy(q_lke_enc_l, bf_b + (Lke + Lenc), sizeof(bf128_t) * (l - (Lke + Lenc)));
+    aes_enc_constraints(lambda, R, Lenc, Senc, in_1, out_1, NULL, NULL, NULL, NULL, 1, q_lke_enc_l,
+                        qk, delta, A0_2, A1_2, B_2);
+    memcpy(B, B_0, (R * 16));
+    memcpy(B + (R * 16), B_1, (R * 16));
+    memcpy(B + (R * 16 * 2), B_2, (R * 16));
   }
 
-  // Step: 18
-  // TODO : unsure what to do
+  // TODO: line 18 and do till end !!
+  // TODO: what is ell ??
   uint8_t* qs;
-
-  // Step: 19
-  // TODO
   uint8_t* q_tilde;
   uint8_t* b_qs_conat;
   uint8_t *r, t;
@@ -908,8 +898,9 @@ bool aes_verify(uint8_t* d, uint8_t* Q, uint8_t* chal_2, uint8_t* chal_3, uint8_
     zk_hash_128(q_tilde, r, chal_2, t, b_qs_conat, ell);
     break;
   }
+
+  // TODO: Do field operation here !!
   uint8_t* q_check = malloc(sizeof(a_tilde));
-  // TODO: its bf operation i guess
   for (uint32_t i = 0; i < sizeof(q_check); i++) {
     q_check[i] = a_tilde[i] + (b_tilde[i] * delta[i]);
   }
