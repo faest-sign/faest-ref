7b37e082886a5fc75932a444936305eff5ee57ab
Sebastian Ramacher <sebastian@ramacher.at>
2023-05-31 01:38:05 +0200

    Test more assumptions

--- tests/instances.cpp
+++ tests/instances.cpp
@@ -22,6 +22,8 @@ BOOST_DATA_TEST_CASE(test_keys, all_parameters, param_id) {
     BOOST_TEST(faest_param.k1 == std::floor((float)faest_param.lambda / faest_param.tau));
     BOOST_TEST(faest_param.t0 == faest_param.lambda % faest_param.tau);
     BOOST_TEST(faest_param.t1 == faest_param.tau - (faest_param.lambda % faest_param.tau));
+    BOOST_TEST(faest_param.Lke % 8 == 0);
+    BOOST_TEST(faest_param.Lenc % 8 == 0);
 
     BOOST_TEST(
         (faest_param.lambda == 128 || faest_param.lambda == 192 || faest_param.lambda == 256));
