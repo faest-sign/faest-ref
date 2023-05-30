1fb48cc818a9b01dbc3b1e28003a3c059e5e581f
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2023-05-30 18:32:49 +0200

    More valgrind

--- crypto_sign_generator.py
+++ crypto_sign_generator.py
@@ -68,9 +68,9 @@ if boost_program_options.found()
 endif
 test_sources = files(join_paths(meson.project_source_root(), 'tests', 'api_test.c'))
 faest_{param_name}_test = executable('faest_{param_name}_api_test', test_sources,
-  dependencies: [libfaest_{param_name}_dependency],
+  dependencies: [libfaest_{param_name}_dependency, valgrind],
   include_directories: include_directories,
-  c_args: defines + c_flags,
+  c_args: defines + c_flags + valgrind_defines,
 )
 test('faest_{param_name}_api_test', faest_{param_name}_test)
 if valgrind.found() and valgrind_exec.found()
