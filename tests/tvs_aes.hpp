d08acbac0e416489775d1da315791f58e08cb5ab
Lennart Braun <lenerd@posteo.de>
2023-06-01 00:30:27 +0200

    std::array

--- tests/tvs_aes.hpp
+++ tests/tvs_aes.hpp
@@ -5,23 +5,23 @@
 #include <cstdint>
 
 namespace aes_ctr_128_tv {
-  extern const uint8_t key[16];
-  extern const uint8_t in[16];
-  extern const uint8_t out[16];
+  extern const std::array<uint8_t, 16> key;
+  extern const std::array<uint8_t, 16> in;
+  extern const std::array<uint8_t, 16> out;
   extern const std::array<uint8_t, 200> expected_extended_witness;
 }
 
 namespace aes_ctr_192_tv {
-  extern const uint8_t key[24];
-  extern const uint8_t in[32];
-  extern const uint8_t out[32];
+  extern const std::array<uint8_t, 24> key;
+  extern const std::array<uint8_t, 32> in;
+  extern const std::array<uint8_t, 32> out;
   extern const std::array<uint8_t, 408> expected_extended_witness;
 }
 
 namespace aes_ctr_256_tv {
-  extern const uint8_t key[32];
-  extern const uint8_t in[32];
-  extern const uint8_t out[32];
+  extern const std::array<uint8_t, 32> key;
+  extern const std::array<uint8_t, 32> in;
+  extern const std::array<uint8_t, 32> out;
   extern const std::array<uint8_t, 500> expected_extended_witness;
 }
 
