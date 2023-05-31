122ae91f1530f28fb0824e7d5807d687d08a5606
Lennart Braun <lenerd@posteo.de>
2023-06-01 00:30:50 +0200

    add missing FAEST_BEGIN_C_DECL

--- faest_aes.h
+++ faest_aes.h
@@ -8,6 +8,8 @@
 
 #include "instances.h"
 
+FAEST_BEGIN_C_DECL
+
 void aes_prove(const uint8_t* w, const uint8_t* u, uint8_t** V, const uint8_t* in,
                const uint8_t* out, const uint8_t* chall, uint8_t* a_tilde, uint8_t* b_tilde,
                const faest_paramset_t* params);
@@ -15,5 +17,7 @@ void aes_prove(const uint8_t* w, const uint8_t* u, uint8_t** V, const uint8_t* i
 uint8_t* aes_verify(uint8_t* d, uint8_t** Q, const uint8_t* chall_2, const uint8_t* chall_3,
                     const uint8_t* a_tilde, const uint8_t* in, const uint8_t* out,
                     const faest_paramset_t* params);
+
+FAEST_END_C_DECL
 
 #endif
