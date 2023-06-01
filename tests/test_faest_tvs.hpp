5a6cd03e82a25c1c135a6e6739e174780dfec05c
Lennart Braun <lenerd@posteo.de>
2023-06-01 14:18:42 +0200

    Add FAEST-EM test vectors

--- tests/test_faest_tvs.hpp
+++ tests/test_faest_tvs.hpp
@@ -51,6 +51,48 @@ namespace faest_256f_tvs {
     extern const std::array<uint8_t, 28400> signature;
 }
 
+namespace faest_em_128s_tvs {
+    extern const std::array<uint8_t, 16 + 16> packed_sk;
+    extern const std::array<uint8_t, 16 + 16> packed_pk;
+    extern const std::array<uint8_t, 16> randomness;
+    extern const std::array<uint8_t, 4566> signature;
+}
+
+namespace faest_em_128f_tvs {
+    extern const std::array<uint8_t, 16 + 16> packed_sk;
+    extern const std::array<uint8_t, 16 + 16> packed_pk;
+    extern const std::array<uint8_t, 16> randomness;
+    extern const std::array<uint8_t, 5696> signature;
+}
+
+namespace faest_em_192s_tvs {
+    extern const std::array<uint8_t, 24 + 24> packed_sk;
+    extern const std::array<uint8_t, 24 + 24> packed_pk;
+    extern const std::array<uint8_t, 24> randomness;
+    extern const std::array<uint8_t, 10824> signature;
+}
+
+namespace faest_em_192f_tvs {
+    extern const std::array<uint8_t, 24 + 24> packed_sk;
+    extern const std::array<uint8_t, 24 + 24> packed_pk;
+    extern const std::array<uint8_t, 24> randomness;
+    extern const std::array<uint8_t, 13912> signature;
+}
+
+namespace faest_em_256s_tvs {
+    extern const std::array<uint8_t, 32 + 32> packed_sk;
+    extern const std::array<uint8_t, 32 + 32> packed_pk;
+    extern const std::array<uint8_t, 32> randomness;
+    extern const std::array<uint8_t, 20956> signature;
+}
+
+namespace faest_em_256f_tvs {
+    extern const std::array<uint8_t, 32 + 32> packed_sk;
+    extern const std::array<uint8_t, 32 + 32> packed_pk;
+    extern const std::array<uint8_t, 32> randomness;
+    extern const std::array<uint8_t, 26736> signature;
+}
+
 }
 
 #endif // TEST_FAEST_TVS_HPP
