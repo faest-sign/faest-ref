b7d04e98964c07d25313a51a068d1f4c73ba87cd
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2023-10-18 00:18:05 +0200

    Extend support for vector_size

--- macros.h
+++ macros.h
@@ -193,7 +193,11 @@
 #endif
 
 /* vector_size attribute */
-#if GNUC_CHECK(4, 8) || __has_attribute(__vector_size__)
+#if GNUC_CHECK(4, 8) || __has_attribute(vector_size)
+#define HAVE_ATTR_VECTOR_SIZE
+#define ATTR_VECTOR_SIZE(s) __attribute__((vector_size(s)))
+#elif __has_attribute(__vector_size__)
+#define HAVE_ATTR_VECTOR_SIZE
 #define ATTR_VECTOR_SIZE(s) __attribute__((__vector_size__(s)))
 #else
 #define ATTR_VECTOR_SIZE(s)
