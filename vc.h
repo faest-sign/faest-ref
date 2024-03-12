b401d5d9d9982cc170678f75524ad1abf4bad404
Johan Degn <degnjohan@gmail.com>
2024-03-12 23:02:39 +0100

    Remove obsolete code from vc and vole

--- vc.h
+++ vc.h
@@ -8,42 +8,7 @@
 
 FAEST_BEGIN_C_DECL
 
-typedef struct vec_com_t {
-  uint8_t* h;
-  uint8_t* k;
-  uint8_t* com;
-  uint8_t* sd;
-} vec_com_t;
-
-typedef struct vec_com_rec_t {
-  uint8_t* h;
-  uint8_t* k;
-  uint8_t* com;
-  uint8_t* s;
-} vec_com_rec_t;
-
-ATTR_CONST uint64_t getBinaryTreeNodeCount(unsigned int depth);
-ATTR_CONST uint64_t getNodeIndex(uint64_t depth, uint64_t levelIndex);
-
-int BitDec(unsigned int leafIndex, unsigned int depth, uint8_t* out);
 unsigned int NumRec(unsigned int depth, const uint8_t* bi);
-
-void vector_commitment(const uint8_t* rootKey, const uint8_t* iv, const faest_paramset_t* params,
-                       uint32_t lambda, vec_com_t* vecCom, uint32_t depth);
-
-void vector_open(const uint8_t* k, const uint8_t* com, const uint8_t* b, uint8_t* pdec,
-                 uint8_t* com_j, uint32_t depth, uint32_t lambdaBytes);
-
-void vector_reconstruction(const uint8_t* iv, const uint8_t* pdec, const uint8_t* com_j,
-                           const uint8_t* b, uint32_t lambda, uint32_t depth,
-                           vec_com_rec_t* vecComRec);
-#if defined(FAEST_TESTS)
-int vector_verify(const uint8_t* iv, const uint8_t* pdec, const uint8_t* com_j, const uint8_t* b,
-                  uint32_t lambda, uint32_t depth, vec_com_rec_t* rec, const uint8_t* vecComH);
-#endif
-
-void vec_com_clear(vec_com_t* com);
-void vec_com_rec_clear(vec_com_rec_t* rec);
 
 FAEST_END_C_DECL
 
--- vole.h
+++ vole.h
@@ -14,19 +14,6 @@ FAEST_BEGIN_C_DECL
 int ChalDec(const uint8_t* chal, unsigned int i, unsigned int k0, unsigned int t0, unsigned int k1,
             unsigned int t1, uint8_t* chalout);
 
-void vole_commit(const uint8_t* rootKey, const uint8_t* iv, unsigned int ellhat,
-                 const faest_paramset_t* params, uint8_t* hcom, vec_com_t* vecCom, uint8_t* c,
-                 uint8_t* u, uint8_t** v);
-
-void vole_reconstruct(const uint8_t* iv, const uint8_t* chal, const uint8_t* const* pdec,
-                      const uint8_t* const* com_j, uint8_t* hcom, uint8_t** q, unsigned int ellhat,
-                      const faest_paramset_t* params);
-
-#if defined(FAEST_TESTS)
-void ConvertToVole(const uint8_t* iv, const uint8_t* sd, bool sd0_bot, unsigned int lambda,
-                   unsigned int depth, unsigned int outLenBytes, uint8_t* u, uint8_t* v);
-#endif
-
 FAEST_END_C_DECL
 
 #endif
