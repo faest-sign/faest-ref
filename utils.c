4413e70d752ff30cf43c9e1390b5e77dbd61239f
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-01-14 16:10:42 +0100

    Add new decoding functions

---
+++ utils.c
@@ -0,0 +1,57 @@
+/*
+ *  SPDX-License-Identifier: MIT
+ */
+
+#if defined(HAVE_CONFIG_H)
+#include <config.h>
+#endif
+
+#include "utils.h"
+
+#include <assert.h>
+
+static inline uint16_t num_rec_2(const uint8_t* v) {
+  uint16_t r;
+  memcpy(&r, v, sizeof(r));
+  return le16toh(r);
+}
+
+bool decode_chall_3(uint8_t* decoded_chall, const uint8_t* chall, unsigned int i,
+                    faest_paramset_t* params) {
+  if (i >= params->faest_param.tau) {
+    return false;
+  }
+
+  const unsigned int t0 = params->faest_param.tau0;
+  const unsigned int t1 = params->faest_param.tau1;
+  const unsigned int k  = params->faest_param.k;
+
+  unsigned int lo;
+  unsigned int hi;
+  if (i < t1) {
+    lo = i * k;
+    hi = (i + 1) * k;
+  } else {
+    const unsigned int t = i - t1;
+
+    lo = (t1 * k) + (t * (k - 1));
+    hi = (t1 * k) + ((t + 1) * (k - 1));
+  }
+
+  assert(hi - lo == k || hi - lo == k - 1);
+  // TODO: this could be implemented more efficiently using bit shifts
+  for (unsigned int j = lo; j < hi; ++j) {
+    ptr_set_bit(decoded_chall, ptr_get_bit(chall, j), j - lo);
+  }
+  return true;
+}
+
+bool decode_all_chall_3(uint16_t* decoded_chall, const uint8_t* chall, faest_paramset_t* params) {
+  for (unsigned int i = 0; i != params->faest_param.tau; ++i) {
+    uint8_t tmp[2];
+    if (!decode_chall_3(tmp, chall, i, params)) {
+      return false;
+    }
+    decoded_chall[i] = num_rec_2(tmp);
+  }
+}
--- utils.h
+++ utils.h
@@ -7,9 +7,11 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "compat.h"
 #include "macros.h"
+#include "instances.h"
 
 FAEST_BEGIN_C_DECL
 
@@ -34,6 +36,12 @@ static inline void masked_xor_u8_array(const uint8_t* a, const uint8_t* b, uint8
   do {                                                                                             \
     (dst)[(index) / 8] |= (value) << ((index) % 8);                                                \
   } while (0)
+
+// DecodeChall_3
+bool decode_chall_3(uint8_t* decoded_chall, const uint8_t* chall, unsigned int i,
+                    faest_paramset_t* params);
+// DecodeAllChall_3
+bool decode_all_chall_3(uint16_t* decoded_chall, const uint8_t* chall, faest_paramset_t* params);
 
 FAEST_END_C_DECL
 
