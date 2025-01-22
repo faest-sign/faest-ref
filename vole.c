15384878089a41ab1afe3bc6131e762322c98703
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-01-22 15:55:11 +0100

    Fix some indices

--- vole.c
+++ vole.c
@@ -26,7 +26,7 @@ static
   const unsigned int k             = params->faest_param.k;
   const unsigned int num_instances = bavc_max_node_index(i, tau_1, k);
   const unsigned int lambda_bytes  = lambda / 8;
-  const unsigned int depth         = i < tau_1 ? k : (k - 1);
+  const unsigned int depth         = bavc_max_node_depth(i, tau_1, k);
 
   // (depth + 1) x num_instances array of outLenBytes; but we only need two rows at a time
   uint8_t* r = calloc(2 * num_instances, outLenBytes);
@@ -48,7 +48,7 @@ static
   memset(v, 0, depth * outLenBytes);
   for (unsigned int j = 0; j < depth; j++) {
     unsigned int depthloop = num_instances >> (j + 1);
-    for (unsigned int idx = 0; idx < depthloop; i++) {
+    for (unsigned int idx = 0; idx < depthloop; idx++) {
       xor_u8_array(V(j), R(j, 2 * idx + 1), V(j), outLenBytes);
       xor_u8_array(R(j, 2 * idx), R(j, 2 * idx + 1), R(j + 1, idx), outLenBytes);
     }
@@ -87,20 +87,20 @@ int ChalDec(const uint8_t* chal, unsigned int i, unsigned int k0, unsigned int t
 }
 
 void vole_commit(const uint8_t* rootKey, const uint8_t* iv, unsigned int ellhat,
-                 const faest_paramset_t* params, vec_com_t* vecCom, uint8_t* c, uint8_t* u,
+                 const faest_paramset_t* params, vec_com_t* bavc, uint8_t* c, uint8_t* u,
                  uint8_t** v) {
   const unsigned int ellhat_bytes = (ellhat + 7) / 8;
   const unsigned int tau          = params->faest_param.tau;
 
-  bavc_commit(rootKey, iv, params, vecCom);
+  bavc_commit(rootKey, iv, params, bavc);
 
   uint8_t* ui = malloc(tau * ellhat_bytes);
 
   unsigned int v_idx = 0;
-  for (unsigned int i = 0; i < tau; i++) {
+  for (unsigned int i = 0; i < tau; ++i) {
     // Step 6
-    v_idx += ConvertToVole(iv, vecCom[i].sd, false, i, ellhat_bytes, ui + i * ellhat_bytes,
-                           v[v_idx], params);
+    v_idx += ConvertToVole(iv, bavc->sd, false, i, ellhat_bytes, ui + i * ellhat_bytes, v[v_idx],
+                           params);
   }
   // Step 9
   memcpy(u, ui, ellhat_bytes);
@@ -114,7 +114,6 @@ void vole_commit(const uint8_t* rootKey, const uint8_t* iv, unsigned int ellhat,
 bool vole_reconstruct(uint8_t* com, uint8_t** q, const uint8_t* iv, const uint8_t* chall_3,
                       const uint8_t* decom_i, const uint8_t* c, unsigned int ellhat,
                       const faest_paramset_t* params) {
-                        
   const unsigned int lambda       = params->faest_param.lambda;
   const unsigned int lambda_bytes = lambda / 8;
   const unsigned int ellhat_bytes = (ellhat + 7) / 8;
@@ -164,8 +163,8 @@ bool vole_reconstruct(uint8_t* com, uint8_t** q, const uint8_t* iv, const uint8_
     } else {
       // Step 14
       for (unsigned int d = 0; d < ki; ++d, ++q_idx) {
-        masked_xor_u8_array(qtmp + q_idx * ellhat_bytes, c + (i - 1) * ellhat_bytes, q[q_idx],
-                            delta[d], ellhat_bytes);
+        masked_xor_u8_array(qtmp + d * ellhat_bytes, c + (i - 1) * ellhat_bytes, q[q_idx], delta[d],
+                            ellhat_bytes);
       }
     }
   }
