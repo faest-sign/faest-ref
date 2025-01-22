c760baf0dace6cb96452aff104ad1c0aa43dcdf8
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-01-22 17:42:17 +0100

    Initial tests for VOLE

--- tests/vole.cpp
+++ tests/vole.cpp
@@ -20,130 +20,75 @@ namespace {
       0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
   };
   constexpr std::array<uint8_t, 16> iv{};
+
+  constexpr unsigned int max_tries = 1000;
 } // namespace
 
 BOOST_AUTO_TEST_SUITE(vole)
 
-BOOST_AUTO_TEST_CASE(chal_dec_base) {
-  uint8_t chal      = 0x42;
-  uint8_t chal_b[8] = {0};
-  ChalDec(&chal, 0, 8, 1, 0, 0, chal_b);
-  BOOST_TEST(NumRec(8, chal_b) == chal);
-}
-
-BOOST_DATA_TEST_CASE(chal_dec, all_parameters, param_id) {
-  BOOST_TEST_CONTEXT("Parameter set: " << faest_get_param_name(param_id)) {
-    const faest_paramset_t params  = faest_get_paramset(param_id);
-    const unsigned int lambda      = params.faest_param.lambda;
-    const unsigned int lambdaBytes = lambda / 8;
-
-    std::vector<uint8_t> chal;
-    chal.resize(lambdaBytes, 0xFF);
-
-    for (unsigned int i = 0; i < params.faest_param.tau; i++) {
-      const unsigned int depth =
-          (i < params.faest_param.t0) ? params.faest_param.k0 : params.faest_param.k1;
-      std::vector<uint8_t> chal_out;
-      chal_out.resize(depth, 0);
-
-      ChalDec(chal.data(), i, params.faest_param.k0, params.faest_param.t0, params.faest_param.k1,
-              params.faest_param.t1, chal_out.data());
-      for (unsigned int j = 0; j != depth; ++j) {
-        BOOST_TEST(chal_out[j] == 1);
-      }
-
-      BOOST_TEST(NumRec(depth, chal_out.data()) == (1 << depth) - 1);
-    }
-  }
-}
-
 BOOST_DATA_TEST_CASE(vole_commit_verify, all_parameters, param_id) {
   BOOST_TEST_CONTEXT("Parameter set: " << faest_get_param_name(param_id)) {
-    const faest_paramset_t params  = faest_get_paramset(param_id);
-    const unsigned int lambda      = params.faest_param.lambda;
-    const unsigned int lambdaBytes = lambda / 8;
+    const faest_paramset_t params   = faest_get_paramset(param_id);
+    const unsigned int lambda       = params.faest_param.lambda;
+    const unsigned int lambda_bytes = lambda / 8;
     const unsigned int ell_hat =
         params.faest_param.l + params.faest_param.lambda * 2 + UNIVERSAL_HASH_B_BITS;
     const unsigned int ell_hat_bytes = (ell_hat + 7) / 8;
+    const auto com_size              = (faest_is_em(&params) ? 2 : 3) * lambda_bytes;
 
-    std::vector<uint8_t> hcom, hcomRec, u, b, chal, c;
-    hcom.resize(lambdaBytes * 2);
-    hcomRec.resize(lambdaBytes * 2);
-    u.resize(ell_hat_bytes);
-    b.resize(MAX(params.faest_param.k0, params.faest_param.k1), 0);
-    chal.resize(lambdaBytes);
-    rand_bytes(chal.data(), chal.size());
+    vec_com_t bavc_com;
+
+    std::vector<uint8_t> chal, c, decom_i, u, q_storage, v_storage;
+    chal.resize(lambda_bytes);
     c.resize((params.faest_param.tau - 1) * ell_hat_bytes);
+    decom_i.resize(com_size * params.faest_param.tau + params.faest_param.T_open * lambda_bytes);
+    u.resize(ell_hat_bytes * params.faest_param.tau);
 
-    std::vector<vec_com_t> vec_com;
-    vec_com.resize(params.faest_param.tau);
-
-    std::vector<uint8_t*> v, q, pdec, com_j;
-    v.resize(lambda);
+    std::vector<uint8_t*> q, v;
     q.resize(lambda);
-    pdec.resize(params.faest_param.tau);
-    com_j.resize(params.faest_param.tau);
+    v.resize(lambda);
 
-    v[0] = new uint8_t[lambda * ell_hat_bytes];
-    q[0] = new uint8_t[lambda * ell_hat_bytes];
+    q_storage.resize(lambda * ell_hat_bytes);
+    v_storage.resize(lambda * ell_hat_bytes);
+
+    q[0] = q_storage.data();
+    v[0] = v_storage.data();
     for (unsigned int i = 1; i < lambda; ++i) {
-      v[i] = v[0] + i * ell_hat_bytes;
       q[i] = q[0] + i * ell_hat_bytes;
+      v[i] = v[0] + i * ell_hat_bytes;
     }
 
-    vole_commit(rootKey.data(), iv.data(), ell_hat, &params, hcom.data(), vec_com.data(), c.data(),
-                u.data(), v.data());
+    vole_commit(rootKey.data(), iv.data(), ell_hat, &params, &bavc_com, c.data(), u.data(),
+                v.data());
 
-    unsigned int running_idx = 0;
-    for (uint32_t i = 0; i < params.faest_param.tau; i++) {
-      const uint32_t depth =
-          (i < params.faest_param.t0) ? params.faest_param.k0 : params.faest_param.k1;
+    std::vector<uint8_t> hcom{bavc_com.h, bavc_com.h + lambda_bytes * 2};
 
-      pdec[i]  = new uint8_t[depth * lambdaBytes];
-      com_j[i] = new uint8_t[lambdaBytes * 2];
-
-      ChalDec(chal.data(), i, params.faest_param.k0, params.faest_param.t0, params.faest_param.k1,
-              params.faest_param.t1, b.data());
-      vector_open(vec_com[i].k, vec_com[i].com, b.data(), pdec[i], com_j[i], depth, lambdaBytes);
-      vec_com_clear(&vec_com[i]);
-    }
-
-    vole_reconstruct(iv.data(), chal.data(), pdec.data(), com_j.data(), hcomRec.data(), q.data(),
-                     ell_hat, &params);
-    BOOST_TEST(hcom == hcomRec);
-    for (unsigned int i = 0; i < params.faest_param.tau; ++i) {
-      const uint32_t depth =
-          (i < params.faest_param.t0) ? params.faest_param.k0 : params.faest_param.k1;
-
-      ChalDec(chal.data(), i, params.faest_param.k0, params.faest_param.t0, params.faest_param.k1,
-              params.faest_param.t1, b.data());
-      for (unsigned int j = 0; j != depth; ++j, ++running_idx) {
-        for (unsigned int inner = 0; inner != ell_hat_bytes; ++inner) {
-          if (b[j]) {
-            // need to correct the vole correlation
-            if (i > 0) {
-              BOOST_TEST((q[(running_idx)][inner] ^ c[(i - 1) * ell_hat_bytes + inner] ^
-                          u[inner]) == v[(running_idx)][inner]);
-            } else {
-              BOOST_TEST((q[(running_idx)][inner] ^ u[inner]) == v[(running_idx)][inner]);
-            }
-          } else {
-            BOOST_TEST(q[(running_idx)][inner] == v[(running_idx)][inner]);
-          }
-        }
+    bool tested = false;
+    for (unsigned int tries = 0; tries != max_tries; ++tries) {
+      rand_bytes(chal.data(), chal.size());
+      for (unsigned int i = lambda - params.faest_param.w_grind; i != lambda; ++i) {
+        ptr_set_bit(chal.data(), 0, i);
       }
-    }
 
-    for (uint32_t i = 0; i < params.faest_param.tau; i++) {
-      delete[] com_j[i];
-      delete[] pdec[i];
-    }
+      uint16_t i_delta[MAX_TAU];
+      BOOST_TEST(decode_all_chall_3(i_delta, chal.data(), &params));
+      if (!bavc_open(&bavc_com, i_delta, decom_i.data(), &params)) {
+        continue;
+      }
 
-    delete[] q[0];
-    delete[] v[0];
+      std::vector<uint8_t> hcom_rec;
+      hcom_rec.resize(lambda_bytes * 2);
+      BOOST_TEST(vole_reconstruct(hcom_rec.data(), q.data(), iv.data(), chal.data(), decom_i.data(),
+                                  c.data(), ell_hat, &params));
+      BOOST_TEST(hcom == hcom_rec);
+      tested = true;
+    }
+    BOOST_TEST(tested);
+    vec_com_clear(&bavc_com);
   }
 }
 
+#if 0
 BOOST_DATA_TEST_CASE(convert_to_vole, all_parameters, param_id) {
   BOOST_TEST_CONTEXT("Parameter set: " << faest_get_param_name(param_id)) {
     const faest_paramset_t params  = faest_get_paramset(param_id);
@@ -197,5 +142,6 @@ BOOST_DATA_TEST_CASE(convert_to_vole, all_parameters, param_id) {
     }
   }
 }
+#endif
 
 BOOST_AUTO_TEST_SUITE_END()
