002ceb7349b6f87e9148c8cc1b35f8c4f69cad4b
Shibam Mukherjee <shimuk8@gmail.com>
2025-02-03 11:59:49 +0100

    fixing

--- faest_aes.c
+++ faest_aes.c
@@ -3030,9 +3030,8 @@ static uint8_t* aes_128_verifier(const uint8_t* d, const uint8_t** Q, const uint
 }
 
 // AES dispatchers
-void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
-               const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
-               const uint8_t* chall_2, const faest_paramset_t* params) {
+void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w, const uint8_t* u, const uint8_t** V, 
+                const uint8_t* owf_in, const uint8_t* owf_out, const uint8_t* chall_2, const faest_paramset_t* params) {
   switch (params->faest_param.lambda) {
   case 256:
     if (params->faest_param.Ske) {
@@ -3057,9 +3056,9 @@ void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const ui
   }
 }
 
-void aes_verify(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* chall_2,
-                const uint8_t* chall_3, const uint8_t* a1_tilde, const uint8_t* a2_tilde,
-                const uint8_t* owf_in, const uint8_t* owf_out, const faest_paramset_t* params) {
+uint8_t* aes_verify(const uint8_t* d, const uint8_t** Q, const uint8_t* chall_2, const uint8_t* chall_3,
+                    const uint8_t* a1_tilde, const uint8_t* a2_tilde, const uint8_t* owf_in, const uint8_t* owf_out,
+                    const faest_paramset_t* params) {
   switch (params->faest_param.lambda) {
   case 256:
     if (params->faest_param.Ske) {
