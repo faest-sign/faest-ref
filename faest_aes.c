7c3f41d3bd7efe607ed9289156d3b8952beea7ed
Lennart Braun <lenerd@posteo.de>
2025-02-03 17:59:44 +0100

    Fix and simplify AES dispatcher

--- faest_aes.c
+++ faest_aes.c
@@ -3528,52 +3528,36 @@ static uint8_t* aes_256_verifier(const uint8_t* d, uint8_t** Q, const uint8_t* o
 
 
 // AES dispatchers
-void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w, const uint8_t* u, uint8_t** V, 
-                const uint8_t* owf_in, const uint8_t* owf_out, const uint8_t* chall_2, const faest_paramset_t* params) {
+void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
+               const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
+               const uint8_t* chall_2, const faest_paramset_t* params) {
   switch (params->faest_param.lambda) {
   case 256:
-    if (params->faest_param.Ske) {
-      aes_256_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params, true);
-    } else {
-      aes_256_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params, false);
-    }
+    aes_256_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params,
+                   params->faest_param.Ske == 0);
     break;
   case 192:
-    if (params->faest_param.Ske) {
-      aes_192_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params, true);
-    } else {
-      aes_192_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params, false);
-    }
+    aes_192_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params,
+                   params->faest_param.Ske == 0);
     break;
   default:
-    if (params->faest_param.Ske) {
-      aes_128_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params, true);
-    } else {
-      aes_128_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params, false);
-    }
+    aes_128_prover(a0_tilde, a1_tilde, a2_tilde, w, u, V, owf_in, owf_out, chall_2, params,
+                   params->faest_param.Ske == 0);
   }
 }
 
-uint8_t* aes_verify(const uint8_t* d, uint8_t** Q, const uint8_t* chall_2, const uint8_t* chall_3, const uint8_t* a1_tilde, const uint8_t* a2_tilde, 
-                      const uint8_t* owf_in, const uint8_t* owf_out, const faest_paramset_t* params) {
+uint8_t* aes_verify(const uint8_t* d, uint8_t** Q, const uint8_t* chall_2, const uint8_t* chall_3,
+                    const uint8_t* a1_tilde, const uint8_t* a2_tilde, const uint8_t* owf_in,
+                    const uint8_t* owf_out, const faest_paramset_t* params) {
   switch (params->faest_param.lambda) {
   case 256: // can also directly pass Ske, but for clarity passing true/false
-    if (params->faest_param.Ske) {
-      return aes_256_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params, true);
-    } else {
-      return aes_256_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params, false);
-    }
+    return aes_256_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params,
+                            params->faest_param.Ske == 0);
   case 192:
-    if (params->faest_param.Ske) {
-      return aes_192_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params, true);
-    } else {
-      return aes_192_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params, false);
-    }
+    return aes_192_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params,
+                            params->faest_param.Ske == 0);
   default:
-    if (params->faest_param.Ske) {
-      return aes_128_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params, true);
-    } else {
-      return aes_128_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params, false);
-    }
+    return aes_192_verifier(d, Q, owf_in, owf_out, chall_2, chall_3, a1_tilde, a2_tilde, params,
+                            params->faest_param.Ske == 0);
   }
 }
