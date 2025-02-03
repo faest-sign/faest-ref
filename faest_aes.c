9496df653bba6287f2e6ffbc8b9a73aa54c154c7
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-02-03 10:23:58 +0100

    Update interface

--- faest_aes.c
+++ faest_aes.c
@@ -2987,10 +2987,10 @@ static uint8_t* aes_128_verifier(const uint8_t* d, uint8_t** Q, const uint8_t* o
 
 }
 
-
 // AES dispatchers
-void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w, const uint8_t* u, uint8_t** V, 
-                const uint8_t* owf_in, const uint8_t* owf_out, const uint8_t* chall_2, const faest_paramset_t* params) {
+void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
+               const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
+               const uint8_t* chall_2, const faest_paramset_t* params) {
   switch (params->faest_param.lambda) {
   case 256:
     if (params->faest_param.Lke) {
@@ -3015,9 +3015,9 @@ void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const ui
   }
 }
 
-uint8_t* aes_verify(const uint8_t* d, uint8_t** Q, const uint8_t* chall_2, const uint8_t* chall_3,
-                    const uint8_t* a1_tilde, const uint8_t* a2_tilde, const uint8_t* owf_in, const uint8_t* owf_out,
-                    const faest_paramset_t* params) {
+void aes_verify(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* chall_2,
+                const uint8_t* chall_3, const uint8_t* a1_tilde, const uint8_t* a2_tilde,
+                const uint8_t* owf_in, const uint8_t* owf_out, const faest_paramset_t* params) {
   switch (params->faest_param.lambda) {
   case 256:
     if (params->faest_param.Lke) {
@@ -3039,4 +3039,3 @@ uint8_t* aes_verify(const uint8_t* d, uint8_t** Q, const uint8_t* chall_2, const
     }
   }
 }
-
