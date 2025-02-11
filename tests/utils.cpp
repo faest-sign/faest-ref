1e26be3d9ef8817ac1ed89f681e9dd59f9af1804
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-02-11 10:35:18 +0100

    Test aligned_alloc wrappers

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -9,12 +9,25 @@
 #include "compat.h"
 
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
 
 BOOST_AUTO_TEST_SUITE(utils)
 
-BOOST_AUTO_TEST_CASE(log2) {
-  const unsigned int depth = 7;
-  BOOST_TEST(ceil_log2(1 << depth) == depth);
+namespace {
+  void test_aligned_alloc(size_t alignment) {
+    void* ptr      = faest_aligned_alloc(alignment, 3 * alignment);
+    uintptr_t iptr = reinterpret_cast<uintptr_t>(ptr);
+    BOOST_TEST((iptr & (alignment - 1)) == 0);
+    faest_aligned_free(ptr);
+  }
+
+  constexpr size_t alignments[3] = {16, 32, 64};
+} // namespace
+
+BOOST_DATA_TEST_CASE(test_faest_aligned_alloc, alignments, alignment) {
+  BOOST_TEST_CONTEXT("alignment: " << alignment) {
+    test_aligned_alloc(alignment);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
