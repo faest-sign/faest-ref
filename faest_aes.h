6eebd725efac0e2e3cbcfb6aa1ad8356082c07cd
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-02-19 19:33:37 +0100

    Move function declaration to the header

--- faest_aes.h
+++ faest_aes.h
@@ -21,6 +21,33 @@ void aes_verify(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t*
                 const uint8_t* chall_3, const uint8_t* a1_tilde, const uint8_t* a2_tilde,
                 const uint8_t* owf_in, const uint8_t* owf_out, const faest_paramset_t* params);
 
+void aes_128_prover(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
+                    const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
+                    const uint8_t* chall_2, const faest_paramset_t* params);
+
+void aes_192_prover(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
+                    const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
+                    const uint8_t* chall_2, const faest_paramset_t* params);
+
+void aes_256_prover(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
+                    const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
+                    const uint8_t* chall_2, const faest_paramset_t* params);
+
+void aes_128_verifier(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* owf_in,
+                      const uint8_t* owf_out, const uint8_t* chall_2, const uint8_t* chall_3,
+                      const uint8_t* a1_tilde, const uint8_t* a2_tilde,
+                      const faest_paramset_t* params);
+
+void aes_192_verifier(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* owf_in,
+                      const uint8_t* owf_out, const uint8_t* chall_2, const uint8_t* chall_3,
+                      const uint8_t* a1_tilde, const uint8_t* a2_tilde,
+                      const faest_paramset_t* params);
+
+void aes_256_verifier(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* owf_in,
+                      const uint8_t* owf_out, const uint8_t* chall_2, const uint8_t* chall_3,
+                      const uint8_t* a1_tilde, const uint8_t* a2_tilde,
+                      const faest_paramset_t* params);
+
 FAEST_END_C_DECL
 
 #endif
--- faest_aes_dispatchers.c
+++ faest_aes_dispatchers.c
@@ -1,31 +1,8 @@
 #include "faest_aes.h"
 
-void aes_128_prover(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
-                    const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
-                    const uint8_t* chall_2, const faest_paramset_t* params);
 
-void aes_192_prover(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
-                    const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
-                    const uint8_t* chall_2, const faest_paramset_t* params);
 
-void aes_256_prover(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
-                    const uint8_t* u, uint8_t** V, const uint8_t* owf_in, const uint8_t* owf_out,
-                    const uint8_t* chall_2, const faest_paramset_t* params);
 
-void aes_128_verifier(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* owf_in,
-                      const uint8_t* owf_out, const uint8_t* chall_2, const uint8_t* chall_3,
-                      const uint8_t* a1_tilde, const uint8_t* a2_tilde,
-                      const faest_paramset_t* params);
-
-void aes_192_verifier(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* owf_in,
-                      const uint8_t* owf_out, const uint8_t* chall_2, const uint8_t* chall_3,
-                      const uint8_t* a1_tilde, const uint8_t* a2_tilde,
-                      const faest_paramset_t* params);
-
-void aes_256_verifier(uint8_t* a0_tilde, const uint8_t* d, uint8_t** Q, const uint8_t* owf_in,
-                      const uint8_t* owf_out, const uint8_t* chall_2, const uint8_t* chall_3,
-                      const uint8_t* a1_tilde, const uint8_t* a2_tilde,
-                      const faest_paramset_t* params);
 
 // AES(-EM) OWF dispatchers
 void aes_prove(uint8_t* a0_tilde, uint8_t* a1_tilde, uint8_t* a2_tilde, const uint8_t* w,
